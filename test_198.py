f356b9bcddd5f4c86be16be78575a0c29334cb4e
Renat Yuldashev <renatyv@gmail.com>
2022-05-04 15:54:56 +0300

    Problem 198. Improved documentation.

--- test_198.py
+++ test_198.py
@@ -1,13 +1,20 @@
 def rob(nums: list[int]) -> int:
     """
+    You are a professional robber planning to rob houses along a street.
+    Each house has a certain amount of money stashed,
+    the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected
+    and it will automatically contact the police if two adjacent houses were broken into on the same night.
+    Given an integer array nums representing the amount of money of each house,
+    return the maximum amount of money you can rob tonight without alerting the police.
+
     Idea:  compute and save max cumulative reward if we rob house #n
-    and use that to compute next one
+    and use that to compute #(n+1)
     """
     if len(nums)<3:
         return max(nums)
     rob_n_max_reward: list[int] = [0, 0] # dummie data
     for ith_reward in nums:
-        # we eithter rob hob previous house and not current, skip
+        # don't rob current house or rob current and skip previous
         rob_n_max_reward.append(max(rob_n_max_reward[-1], rob_n_max_reward[-2]+ith_reward))
     return rob_n_max_reward[-1]
 
