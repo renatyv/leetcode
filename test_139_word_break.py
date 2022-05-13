b80e8c65fa1e79b4aebfefcf55b4ebf5297fd489
Renat Yuldashev <renatyv@gmail.com>
2022-05-13 18:56:46 +0300

    Simpler solution for 139. Ideas: recursion (dynamic programming) + cache (memoization).

--- test_139_word_break.py
+++ test_139_word_break.py
@@ -1,3 +1,6 @@
+from functools import cache
+
+
 def wordBreak(s: str, wordDict: list[str]) -> bool:
     """Given a string s and a dictionary of strings wordDict,
     return true if s can be segmented into a space-separated sequence of one or more dictionary words.
@@ -10,27 +13,21 @@ def wordBreak(s: str, wordDict: list[str]) -> bool:
     2. Do not check the same substring s[i:] twice.
     To do that, keep track of all substring starting indexes we checked for
     3. Iterate over all possible prefix lengths for s[i:] to check that they are in the word_set"""
-    if len(s) == 0:
-        return True
-    # start symbol number, current word number
-    word_set = set(wordDict)
-    word_lengths: set[int] = {len(word) for word in wordDict}
-    asc_sorted_w_lengths = list(sorted(word_lengths))
-    starting_index_scheduled = [False] * (len(s)+1)
-    starting_index_scheduled[0] = True
-    stack = [0]
-    while stack:
-        start_symbol = stack.pop()
-        if start_symbol == len(s):
+    words_set = set(wordDict)
+
+    @cache
+    def recursive(start_index: int) -> bool:
+        nonlocal words_set
+        nonlocal s
+        if start_index == len(s):
             return True
-        for prefix_len in asc_sorted_w_lengths:
-            end = start_symbol+prefix_len
-            if end <= len(s)\
-                    and not starting_index_scheduled[end]\
-                    and s[start_symbol:end] in word_set:
-                stack.append(end)
-                starting_index_scheduled[end] = True
-    return False
+        for word in words_set:
+            if s.startswith(word, start_index):
+                if recursive(start_index + len(word)):
+                    return True
+        return False
+
+    return recursive(0)
 
 
 def test_examples():
@@ -43,9 +40,9 @@ def test_corner():
     assert wordBreak("a", ["a"])
     assert wordBreak("aa", ["a"])
     assert wordBreak("aaa", ["a"])
-    assert wordBreak("aaaaaaab", ["a","ab"])
+    assert wordBreak("aaaaaaab", ["a", "ab"])
     assert wordBreak("ab", ["ab"])
-    assert wordBreak("aab",["a","aab"])
+    assert wordBreak("aab", ["a", "aab"])
     assert wordBreak("abab", ["ab"])
     assert wordBreak("ab", ["a", "b"])
     assert wordBreak("ab", ["b", "a"])
@@ -56,4 +53,4 @@ def test_corner():
     assert not wordBreak("a", ["b"])
     assert not wordBreak("a", ["b", "c"])
     assert not wordBreak("abcd", ["efg", "bcd"])
-    assert not wordBreak("abc", ["a","b"])
+    assert not wordBreak("abc", ["a", "b"])
