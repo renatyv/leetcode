68588463f6b6d26e0d33b39f3479df6826c09d15
Renat Yuldashev <renatyv@gmail.com>
2022-05-14 16:53:05 +0300

    Reformat file for problem 47

--- test_47_permutations_2.py
+++ test_47_permutations_2.py
@@ -21,11 +21,12 @@ def permuteUnique(nums: list[int]) -> list[list[int]]:
                 nums_counter[num] -= 1
                 current_prefix.append(num)
                 for perm in generate_permutations(current_prefix,
-                                      nums_counter,
-                                      required_length):
+                                                  nums_counter,
+                                                  required_length):
                     yield perm
                 nums_counter[num] += 1
                 current_prefix.pop()
+
     nums_count = Counter(nums)
     return list(generate_permutations([], nums_count, len(nums)))
 
@@ -33,32 +34,36 @@ def permuteUnique(nums: list[int]) -> list[list[int]]:
 def compare_permutations(permutations1, permutations2):
     def nums_to_str(nums):
         return ''.join(chr(ord('a') + 10 + num) for num in nums)
+
     set1 = {nums_to_str(perm) for perm in permutations1}
     set2 = {nums_to_str(perm) for perm in permutations2}
     return set1 == set2
 
 
 def test_examples():
-    assert compare_permutations(permuteUnique([1,1,2]), [[2,1,1],[1,2,1],[1,1,2]])
-    assert compare_permutations(permuteUnique([1,2,3]), [[3, 2, 1], [2, 3, 1], [2, 1, 3], [3, 1, 2], [1, 3, 2], [1, 2, 3]])
+    assert compare_permutations(permuteUnique([1, 1, 2]), [[2, 1, 1], [1, 2, 1], [1, 1, 2]])
+    assert compare_permutations(permuteUnique([1, 2, 3]),
+                                [[3, 2, 1], [2, 3, 1], [2, 1, 3], [3, 1, 2], [1, 3, 2], [1, 2, 3]])
 
 
 def test_corner_cases():
     assert permuteUnique([1]) == [[1]]
-    assert permuteUnique([1,1]) == [[1,1]]
+    assert permuteUnique([1, 1]) == [[1, 1]]
     assert permuteUnique([1, 1, 1, 1]) == [[1, 1, 1, 1]]
 
 
 def test_cases_2():
     assert permuteUnique([1, 1]) == [[1, 1]]
-    assert compare_permutations(permuteUnique([1, 2]), [[2, 1],[1, 2]])
+    assert compare_permutations(permuteUnique([1, 2]), [[2, 1], [1, 2]])
 
 
 def test_cases_3():
-    assert permuteUnique([1,1,1]) == [[1,1,1]]
-    assert compare_permutations(permuteUnique([2, 1, 1]), [[2,1,1],[1,2,1],[1,1,2]])
-    assert compare_permutations(permuteUnique([3, 1, 2]), [[3, 2, 1], [2, 3, 1], [2, 1, 3], [3, 1, 2], [1, 3, 2], [1, 2, 3]])
+    assert permuteUnique([1, 1, 1]) == [[1, 1, 1]]
+    assert compare_permutations(permuteUnique([2, 1, 1]), [[2, 1, 1], [1, 2, 1], [1, 1, 2]])
+    assert compare_permutations(permuteUnique([3, 1, 2]),
+                                [[3, 2, 1], [2, 3, 1], [2, 1, 3], [3, 1, 2], [1, 3, 2], [1, 2, 3]])
 
 
 def test_cases_4():
-    assert compare_permutations(permuteUnique([1,1,2,2]), [[2,2,1,1],[1,2,2,1],[1,1,2,2],[1,2,1,2],[2,1,2,1],[2,1,1,2]])
+    assert compare_permutations(permuteUnique([1, 1, 2, 2]),
+                                [[2, 2, 1, 1], [1, 2, 2, 1], [1, 1, 2, 2], [1, 2, 1, 2], [2, 1, 2, 1], [2, 1, 1, 2]])
