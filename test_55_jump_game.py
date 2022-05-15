737ed62bf7220da9ad748562188aa618b7fac514
Renat Yuldashev <renatyv@gmail.com>
2022-05-15 19:07:09 +0300

    Problem 55: minor refactorings.

--- test_55_jump_game.py
+++ test_55_jump_game.py
@@ -7,28 +7,29 @@ def canJump_recursive(nums: list[int]) -> bool:
     if nums[0] == 0:
         return False
     visited = [False] * len(nums)
+
     def recursive_try_jump(cur_step: int, nums: list[int], visited: list[bool]):
-        if cur_step >= len(nums)-1:
+        if cur_step >= len(nums) - 1:
             return True
         if nums[cur_step] == 0:
             return False
         if visited[cur_step]:
             return False
         visited[cur_step] = True
-        max_step = cur_step+nums[cur_step]
+        max_step = cur_step + nums[cur_step]
         for next_step in range(max_step,
                                cur_step,
                                -1):
             if recursive_try_jump(next_step, nums, visited):
                 return True
         return False
+
     return recursive_try_jump(0, nums, visited)
 
 
 def canJump(nums: list[int]) -> bool:
     """Idea: create bool array of reachable steps
-    Idea 2: try jumping the longest distance first
-    Idea 3: use bitmasks instead of bool array"""
+    Idea: DFS"""
     if len(nums) == 1:
         return True
     if nums[0] == 0:
@@ -39,31 +40,27 @@ def canJump(nums: list[int]) -> bool:
         cur_step = unvisited_indexes_stack.pop()
         if visited[cur_step]:
             continue
-        if nums[cur_step] == 0:
-            visited[cur_step] = True
-            continue
         visited[cur_step] = True
-        max_index = cur_step+nums[cur_step]
-        if max_index >= len(nums)-1:
+        max_index = cur_step + nums[cur_step]
+        if max_index >= len(nums) - 1:
             return True
-        for next_step in range(cur_step+1, max_index+1):
+        for next_step in range(cur_step + 1, max_index + 1):
             if not visited[next_step]:
                 unvisited_indexes_stack.append(next_step)
     return False
 
 
 def test_canJump_1():
-    assert not canJump([1,0,2,3,4])
+    assert not canJump([1, 0, 2, 3, 4])
 
 
 def test_canJump():
     assert canJump([0])
     assert canJump([1])
-    assert canJump([2,3,1,1,4])
-    assert canJump([1,1,1,1,1])
-    assert canJump([2,4,0,1,0,0])
+    assert canJump([2, 3, 1, 1, 4])
+    assert canJump([1, 1, 1, 1, 1])
+    assert canJump([2, 4, 0, 1, 0, 0])
     assert not canJump([0, 1])
     assert not canJump([1, 0, 0])
     assert not canJump([2, 4, 0, 2, 0, 0, 0])
-    assert not canJump([3,2,1,0,4])
-
+    assert not canJump([3, 2, 1, 0, 4])
