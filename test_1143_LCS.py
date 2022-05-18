7490ae0056dd01b313b5510df8bb6ed9fe8228b8
Renat Yuldashev <renatyv@gmail.com>
2022-05-18 09:55:23 +0300

    Problem 1143. Classical longest common subsequence. Optimize for space, saving only the necessary data from previous steps.

--- test_1143_LCS.py
+++ test_1143_LCS.py
@@ -6,26 +6,31 @@ def longestCommonSubsequence(text1: str, text2: str) -> int:
     Idea: 2d dynamic programming
     1) LCS(s1+X,s2+X) = LCS(s1,s2)+X
     2) LCS(s1+Y,s2+Y) = max(LCS(s1, s2+Y), LCS(s1+X,s2))"""
-    lcs_array: list[list[str]] = [[''] * (len(text1)+1) for row in range(len(text2)+1)]
-    for t2 in range(1, len(text2)+1): # iterate over rows
-        for t1 in range(1, len(text1)+1): # iterate over columns
-            if text1[t1-1] == text2[t2-1]:
-                lcs_array[t2][t1] = lcs_array[t2-1][t1-1] + text1[t1-1]
+    prev_row = [''] * (len(text1) + 1)
+    # lcs_array: list[list[str]] = [[''] * (len(text1)+1) for row in range(len(text2)+1)]
+    for t2 in range(1, len(text2) + 1):  # iterate over rows
+        cur_row = [''] * (len(text1) + 1)
+        prev_col = ''
+        for t1 in range(1, len(text1) + 1):  # iterate over columns
+            if text1[t1 - 1] == text2[t2 - 1]:
+                cur_row[t1] = prev_row[t1 - 1] + text1[t1 - 1]
             else:
-                if len(lcs_array[t2-1][t1]) > len(lcs_array[t2][t1-1]):
-                    lcs_array[t2][t1] = lcs_array[t2-1][t1]
+                if len(prev_row[t1]) > len(prev_col):
+                    cur_row[t1] = prev_row[t1]
                 else:
-                    lcs_array[t2][t1] = lcs_array[t2][t1-1]
-    return len(lcs_array[len(text2)][len(text1)])
+                    cur_row[t1] = prev_col
+            prev_col = cur_row[t1]
+        prev_row = cur_row
+    return len(prev_row[-1])
 
 
 def test_corner_cases():
-    assert longestCommonSubsequence('a','a') == len('a')
+    assert longestCommonSubsequence('a', 'a') == len('a')
     assert longestCommonSubsequence('ab', 'ab') == len('ab')
     assert longestCommonSubsequence('acb', 'ab') == len('ab')
 
 
 def test_examples():
-    assert longestCommonSubsequence(text1 = "abcde", text2 = "ace" ) == len('ace')
-    assert longestCommonSubsequence(text1 = "abc", text2 = "abc") == len('abc')
-    assert longestCommonSubsequence(text1 = "abc", text2 = "def") == len('')
+    assert longestCommonSubsequence(text1="abcde", text2="ace") == len('ace')
+    assert longestCommonSubsequence(text1="abc", text2="abc") == len('abc')
+    assert longestCommonSubsequence(text1="abc", text2="def") == len('')
