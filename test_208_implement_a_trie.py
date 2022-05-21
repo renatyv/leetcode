35dca09489b15600e1f2b0942b8421771c66ac16
Renat Yuldashev <renatyv@gmail.com>
2022-05-21 11:11:15 +0300

    Problem 208. Make fields and inner class protected

--- test_208_implement_a_trie.py
+++ test_208_implement_a_trie.py
@@ -1,18 +1,18 @@
 class Trie:
-    class Node:
+    class _Node:
         def __init__(self, children: dict, is_terminal: bool):
             self.children = children
             self.is_terminal = is_terminal
 
     def __init__(self):
-        self.head = Trie.Node(dict(), False)
+        self._head = Trie._Node(dict(), False)
 
     def insert(self, word: str) -> None:
         """ Inserts the string word into the trie."""
         if not word:
-            self.head.is_terminal = True
+            self._head.is_terminal = True
         else:
-            cur_node = self.head
+            cur_node = self._head
             char_index = 0
             while char_index < len(word) and word[char_index] in cur_node.children:
                 cur_node = cur_node.children[word[char_index]]
@@ -21,7 +21,7 @@ class Trie:
                 cur_node.is_terminal = True
             else:
                 while char_index < len(word):
-                    new_node = Trie.Node(dict(), False)
+                    new_node = Trie._Node(dict(), False)
                     cur_node.children[word[char_index]] = new_node
                     cur_node = new_node
                     char_index += 1
@@ -32,8 +32,8 @@ class Trie:
         :returns true if the string word is in the trie (i.e., was inserted before),
                 false otherwise."""
         if word == '':
-            return self.head.is_terminal
-        cur_node = self.head
+            return self._head.is_terminal
+        cur_node = self._head
         try:
             for char in word:
                 cur_node = cur_node.children[char]
@@ -46,9 +46,9 @@ class Trie:
         :returns true if there is a previously inserted string word that has the prefix,
                 false otherwise."""
         if prefix == '':
-            return not (self.head.children is None)
+            return not (self._head.children is None)
         try:
-            cur_node = self.head
+            cur_node = self._head
             for char in prefix:
                 cur_node = cur_node.children[char]
         except KeyError:
