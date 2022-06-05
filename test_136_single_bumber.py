74c81846592bd4ad5f60ea8df20a34dfe1eb2563
Renat Yuldashev <renatyv@gmail.com>
2022-06-05 16:13:00 +0300

    Problem 136. Idea: XOR.

--- test_136_single_bumber.py
+++ test_136_single_bumber.py
@@ -9,19 +9,17 @@
 #         else:
 #             nums_set.remove(num)
 #     return nums_set.pop()
+import functools
 
 
 def singleNumber(nums: list[int]) -> int:
     """Given a non-empty array of integers nums, every element appears twice except for one.
     Find that single one.
     Idea: use XOR"""
-    res = 0
-    for num in nums:
-        res = res ^ num
-    return res
+    return functools.reduce(lambda a, b: a ^ b, nums)
 
 
 def test_singleNumber_examples():
-    assert singleNumber([2,2,1]) == 1
-    assert singleNumber([4,1,2,1,2]) == 4
+    assert singleNumber([2, 2, 1]) == 1
+    assert singleNumber([4, 1, 2, 1, 2]) == 4
     assert singleNumber([1]) == 1
