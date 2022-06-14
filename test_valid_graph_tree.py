532c9b34952c9d4f157caaad56b43bc8dae76fb4
Renat Yuldashev <renatyv@gmail.com>
2022-06-14 19:28:56 +0300

    Problem 178 lintcode. Union-find solution using path compression. Use list of parents, parent[node] = root

--- test_valid_graph_tree.py
+++ test_valid_graph_tree.py
@@ -1,5 +1,7 @@
 def valid_tree(n: int, edges: list[list[int]]) -> bool:
     """
+    solution: https://iq.opengenus.org/union-find/
+    :param n: number of nodes in a graph
     https://www.lintcode.com/problem/178/description
     Given n nodes labeled from 0 to n - 1 and a list of undirected edges (each edge is a pair of nodes),
     write a function to check whether these edges make up a valid tree.
@@ -12,25 +14,35 @@ def valid_tree(n: int, edges: list[list[int]]) -> bool:
     if len(edges) != n - 1:
         return False
 
-    trees = [{k} for k in range(n)]
+    # union-find data structure to store disjoint sets
+    # parents[child] == parent
+    # parents[root] == root
+    parents = list(range(n))
 
-    def find_tree_index(trees: list[set[int]], node: int) -> int:
-        for tree_index, tree in enumerate(trees):
-            if node in tree:
-                return tree_index
+    def find_root_compressing_paths(parents: list[int], node: int) -> int:
+        """find root starting from the node with path compression"""
+        path = [node]
+        # find root saving the path
+        while parents[path[-1]] != path[-1]:
+            path.append(parents[path[-1]])
+        root = path[-1]
+        # compress path to root
+        for node in path:
+            parents[node] = root
+        return root
 
     for edge in edges:
         node1, node2 = edge
-        # find tree for node1
-        node1_tree_index = find_tree_index(trees, node1)
-        node2_tree_index = find_tree_index(trees, node2)
-        # cycle
-        if node1_tree_index == node2_tree_index:
+        # find roots for node1 and node2
+        node1_root = find_root_compressing_paths(parents, node1)
+        node2_root = find_root_compressing_paths(parents, node2)
+        # if they are already in a tree, then there must be a cycle
+        if node1_root == node2_root:
             return False
-        # connect trees into one
-        trees[node1_tree_index] = trees[node1_tree_index].union(trees[node2_tree_index])
-        del trees[node2_tree_index]
-    return len(trees) == 1
+        # Union trees into one
+        parents[node2_root] = node1_root
+    # there are no cycles and number of edges == number of nodes -1
+    return True
 
 
 def test_edge_cases():
@@ -39,6 +51,11 @@ def test_edge_cases():
     assert valid_tree(2, [[0, 1]])
     assert not valid_tree(2, [[0, 1], [1, 0]])
     assert not valid_tree(2, [[0, 0], [1, 0]])
+    assert not valid_tree(8, [[0, 1], [0, 2], [2, 3], [3, 0], [0, 5], [0, 6], [7, 0]])
+
+
+def test_wa1():
+    assert valid_tree(8, [[0, 1], [0, 2], [3, 0], [4, 0], [0, 5], [0, 6], [7, 0]])
 
 
 def test_examples():
